6a740c0b186d2a7d90b4a4ac47823a793f34fd74
Dan Bearl <dan.bearl@gmail.com>
2013-02-25 22:51:15 -0500

    Add methods to Reservation model
    - #payment_confirmed toggles paid to true
    - #generate_random_id generates a 15-digit random alphanumeric string

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,27 @@
 class Reservation < ActiveRecord::Base
-  attr_accessible :address, :age, :camp_start_date, :date_signed, :email, :emergency_contact, :guardian_signature, :hold_harmless_signature, :insurance, :medical_provider, :name, :parental_consent_signature, :phone
+
+  attr_accessible :address, :age, :camp_start_date, :date_signed, :email, :emergency_contact, :guardian_signature, :hold_harmless_signature, :insurance, :medical_provider, :name, :parental_consent_signature, :phone, :confirmation_id, :paid, :camp_type, :camp_price
 
   validates_presence_of :name, :age, :phone, :insurance, :parental_consent_signature, :hold_harmless_signature, :guardian_signature, :date_signed, :camp_start_date
+
+  def payment_confirmed
+    write_attribute :paid, true
+    save!
+
+  end
+
+  def self.generate_random_id
+    token = []
+    options = Array('a'..'z') + Array(0..9) + Array(0..9) + Array('A'..'Z')
+
+    15.times do
+      token << options.sample
+    end
+
+    token = token.join('')
+
+    return token
+
+  end
+
 end
