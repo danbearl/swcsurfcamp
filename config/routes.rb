006d847841bac8fd2c8967af35a623d7c72c897f
Dan Bearl <dan.bearl@gmail.com>
2013-02-25 22:54:16 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Swcsurfcamp::Application.routes.draw do
 
+  get "camps/new"
+
+  get "camps/index"
+
+  get "camps/show"
+
   get "welcome/index"
+  get "paypal_express/checkout"
+  get "paypal_express/review"
+  get "paypal_express/purchase"
 
   resources :reservations
+  resources :camps
+  resources :pages, only: [:new, :create]
 
   root :to => 'welcome#index'
-
-  resources :pages, only: [:new, :create]
 
   #keep these at the bottom of your file. They should be the last routes.
   get "/:slug", to: "pages#show", as: :slug
