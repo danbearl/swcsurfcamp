4be282b06221d3f3f46cb5b670e5ab36bb6797a8
Dan Bearl <dan.bearl@gmail.com>
2013-03-27 11:19:28 -0400

    Redirect unrecognized slugs to 404.html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,12 @@ class PagesController < ApplicationController
   # the navigation in your Application Layout
   expose(:page_slugs ) { Page.select(:slug).map(&:slug) }
 
+  def show
+    if page.is_a? NilClass
+      redirect_to "/404.html"
+    end
+  end
+
   def create
     if new_page.save
       redirect_to slug_path(new_page.slug)
